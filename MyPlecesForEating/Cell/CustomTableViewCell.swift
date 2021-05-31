157a9be647cfd6046ce00f1074b6868a797171e9
nikita_kogolenok <nikiton17ns@gmail.com>
2021-05-31 15:58:47 +0300

    Custom Table View Cell

---
+++ MyPlecesForEating/Cell/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlecesForEating
+//
+//  Created by Никита Коголенок on 31.05.21.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    // MARK: - Outlet
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+}
