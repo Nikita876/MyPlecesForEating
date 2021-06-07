d718d2cedc584bf9a6543b75233d7875043fb963
nikita_kogolenok <nikiton17ns@gmail.com>
2021-06-07 11:13:21 +0300

    Pass data between controllers Add new image

--- MyPlecesForEating/Controllers/NewPlaceViewControllerController.swift
+++ MyPlecesForEating/Controllers/NewPlaceViewControllerController.swift
@@ -8,16 +8,24 @@
 import UIKit
 
 class NewPlaceViewControllerController: UITableViewController {
+    // MARK: - Variables
+    var newPlace: Place?
+    var imageIsChanged = false
     // MARK: - Outlet
-    @IBOutlet weak var imageOfPlace: UIImageView!
-    
+    @IBOutlet weak var placeImage: UIImageView!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var placeName: UITextField!
+    @IBOutlet weak var placeLocation: UITextField!
+    @IBOutlet weak var placeType: UITextField!
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.tableFooterView = UIView()
+        saveButton.isEnabled = false
+        
+        placeName.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
     }
-    
     // MARK: - Table view delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
@@ -53,18 +61,48 @@ class NewPlaceViewControllerController: UITableViewController {
             view.endEditing(true)
         }
     }
+    
+    func saveNewPlace() {
+        
+        var image: UIImage?
+        
+        if imageIsChanged {
+            image = placeImage.image
+        } else {
+            image = #imageLiteral(resourceName: "imagePlaceholder")
+        }
+        
+        newPlace = Place(name: placeName.text!,
+                         location: placeLocation.text,
+                         type: placeType.text,
+                         image: image,
+                         restaurantImage: nil)
+    }
+    // MARK: - Action
+    @IBAction func cancelAction(_ sender: Any) {
+        dismiss(animated: true)
+    }
 }
-
 // MARK: - Text field delegate
 extension NewPlaceViewControllerController: UITextFieldDelegate {
+    // MARK: - Methods
     // Скрываем кдавиатуру по нажатию Done
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
+    
+    @objc private func textFieldChanged() {
+        if placeName.text?.isEmpty == false {
+            saveButton.isEnabled = true
+        } else {
+            saveButton.isEnabled = false
+        }
+    }
 }
 // MARK: - Work with image
 extension NewPlaceViewControllerController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // MARK: - Methods
     func chooseImagePicker(sourse: UIImagePickerController.SourceType) {
         
         if UIImagePickerController.isSourceTypeAvailable(sourse) {
@@ -76,11 +114,14 @@ extension NewPlaceViewControllerController: UIImagePickerControllerDelegate, UIN
         }
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
-        imageOfPlace.image = info[.editedImage] as? UIImage
-        imageOfPlace.contentMode = .scaleAspectFill
-        imageOfPlace.clipsToBounds = true
+        placeImage.image = info[.editedImage] as? UIImage
+        placeImage.contentMode = .scaleAspectFill
+        placeImage.clipsToBounds = true
+        
+        imageIsChanged = true
         dismiss(animated: true)
     }
 }
--- MyPlecesForEating/Model/PlaceModel.swift
+++ MyPlecesForEating/Model/PlaceModel.swift
@@ -5,13 +5,14 @@
 //  Created by Никита Коголенок on 31.05.21.
 //
 
-import Foundation
+import UIKit
 
 struct Place {
     var name: String
-    var location: String
-    var type: String
-    var image: String
+    var location: String?
+    var type: String?
+    var image: UIImage?
+    var restaurantImage: String?
     
     static let restaurantNames = ["Burger Heroes", "Kitchen", "Bonsai", "Дастархан", "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes", "Speak Easy", "Morris Pub", "Вкусные истории", "Классик", "Love&Life", "Шок", "Бочка"]
     
@@ -19,7 +20,7 @@ struct Place {
         var places = [Place]()
         
         for place in restaurantNames {
-            places.append(Place(name: place, location: "Уфа", type: "Ресторан", image: place))
+            places.append(Place(name: place, location: "Уфа", type: "Ресторан", image: nil, restaurantImage: place))
         }
         
         return places
